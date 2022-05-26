84559d66d555bdb7af33161304ef6c288ed01431
Carolina <caroliina20003@gmail.com>
2022-05-25 23:05:43 -0500

    BUILD

--- src/app/feature/registro/componentes/home-registro/home-registro.component.ts
+++ src/app/feature/registro/componentes/home-registro/home-registro.component.ts
@@ -6,14 +6,11 @@ import { RegistroService } from '../../shared/services/registro.service';
   templateUrl: './home-registro.component.html',
   styleUrls: ['./home-registro.component.scss']
 })
-export class HomeRegistroComponent implements OnInit {
+export class HomeRegistroComponent {
 
   form = false;
 
   constructor(private registroService: RegistroService) { }
-
-  ngOnInit(): void {
-  }
 
   async getRegistro(ev: any) {
     const value = await this.registroService.add(ev.value).toPromise();
